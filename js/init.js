c48dc1b917034b7b580b61bdfad299186217db27
Elia Cereda <eliacereda@gmail.com>
2015-03-12 00:52:36 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -13,8 +13,8 @@ jQuery(function($) {
 			global:		{ range: '*', href: 'css/style.css', containers: 1400, grid: { gutters: 50 } },
 			wide:		{ range: '-1680', href: 'css/style-wide.css', containers: 1200, grid: { gutters: 40 } },
 			normal:		{ range: '-1280', href: 'css/style-normal.css', containers: 960, viewport: { scalable: false } },
-			narrow:		{ range: '-1030', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
-			narrower:	{ range: '-860', href: 'css/style-narrower.css', containers: '95%!' },
+			narrow:		{ range: '-1100', href: 'css/style-narrow.css', containers: '95%', grid: { gutters: 30 } },
+			narrower:	{ range: '-1030', href: 'css/style-narrower.css', containers: '95%!' },
 			mobile:		{ range: '-736', href: 'css/style-mobile.css', containers: '100%!' }
 		},
 		plugins: {
